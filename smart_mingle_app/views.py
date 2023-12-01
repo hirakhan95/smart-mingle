e4c945ef40ea74dc206dd14d82c77dd82274138f
Hira Khan <phr.hirakhan@gmail.com>
2023-12-01 18:00:49 +0100

    render home page view

--- smart_mingle_app/views.py
+++ smart_mingle_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-def get_main_page(request):
-    return render(request, 'smart_mingle/index.html')
+def home(request):
+    return render(request, 'home.html')
