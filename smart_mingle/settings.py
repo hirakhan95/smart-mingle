e5a739c022b3f403fdd77fddaf5fa263f73f35bc
Hira Khan <phr.hirakhan@gmail.com>
2023-12-01 17:57:58 +0100

    add staticfile directory to load css

--- smart_mingle/settings.py
+++ smart_mingle/settings.py
@@ -93,7 +93,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -131,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
