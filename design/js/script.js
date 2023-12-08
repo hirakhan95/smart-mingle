2a6be7c4565d198cc5fa399dac5eb71f663d6449
Hira Khan <phr.hirakhan@gmail.com>
2023-12-08 13:58:53 +0100

    calendar and date are now designed more appropriatelay using boostrstrap 5

--- design/js/script.js
+++ design/js/script.js
@@ -1,3 +1,9 @@
-$(function () {
-  $("#datetimepicker1").datetimepicker({});
-});
+// $(function () {
+//   $("#datetimepicker1").datetimepicker({});
+// });
+
+var dateEl = document.getElementById("date");
+var timeEl = document.getElementById("time");
+
+document.getElementById("date-output").innerHTML = dateEl.type === "date";
+document.getElementById("time-output").innerHTML = timeEl.type === "time";
