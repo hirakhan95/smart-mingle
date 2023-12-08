35fecc23d9079284f6024422f6732aaf55cf969a
Hira Khan <phr.hirakhan@gmail.com>
2023-12-08 15:23:47 +0100

    initiate tiny in javascript I am using specific plugins removed menubar and selective toolbar for it

--- design/js/script.js
+++ design/js/script.js
@@ -1,9 +1,21 @@
-var dateEl = document.getElementById("date");
-var timeEl = document.getElementById("time");
-
-document.getElementById("date-output").innerHTML = dateEl.type === "date";
-document.getElementById("time-output").innerHTML = timeEl.type === "time";
-
+//
 tinymce.init({
-  selector: "#mytextarea",
+  menubar: false,
+  selector: "textarea",
+  plugins:
+    "autolink charmap codesample emoticons link lists table visualblocks wordcount checklist casechange export permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+  toolbar:
+    "blocks fontfamily fontsize | bold italic underline | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist | emoticons charmap | removeformat",
+  toolbar_mode: "sliding",
+  elementpath: false,
+  tinycomments_mode: "embedded",
+  placeholder: "Whats your event about!",
+  branding: false,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var today = new Date().toISOString().split("T")[0];
+  var dateInput = document.getElementById("date");
+  dateInput.setAttribute("min", today);
+  dateInput.value = today;
 });
