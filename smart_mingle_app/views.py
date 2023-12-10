a13ba5f7e2a7d6a4ff9663b9705ee8b5c9db2303
Hira Khan <phr.hirakhan@gmail.com>
2023-12-10 16:27:12 +0100

    get post excepted on home view

--- smart_mingle_app/views.py
+++ smart_mingle_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET", "POST"])
 def home(request):
     return render(request, 'home.html')
 
