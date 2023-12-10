da02512686e46e4cfbaf0be26188e49f7a924145
Hira Khan <phr.hirakhan@gmail.com>
2023-12-10 20:51:20 +0100

    cutsom signup form written for first_name and last_name

--- smart_mingle_app/forms.py
+++ smart_mingle_app/forms.py
@@ -1,4 +1,6 @@
+from allauth.account.forms import SignupForm
 from django import forms
+
 from .models import Event
 
 
@@ -6,3 +8,14 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'description', 'date', 'venue']
+
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
