3c8e0dffea2a513f865ba60a04c6941d7d1a974a
Hira Khan <phr.hirakhan@gmail.com>
2023-12-19 00:48:20 +0100

    models.py: more fields added to Event class

--- smart_mingle_app/models.py
+++ smart_mingle_app/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -20,10 +19,14 @@ class Contact(models.Model):
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80, unique=True)
+    img_url = models.CharField(max_length=250)
     description = models.TextField()
-    date = models.DateField()
-    venue = models.CharField(max_length=200)
+    location = models.CharField(max_length=50)
+    category = models.CharField(max_length=15)
+    start_time = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
