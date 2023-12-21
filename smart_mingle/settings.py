cea705acb720cd29813af1c14f5aeef40d98270c
Hira Khan <phr.hirakhan@gmail.com>
2023-12-21 22:43:44 +0100

    EMAIL_BACKEND and debug are now taken on basis of local machine and environment

--- smart_mingle/settings.py
+++ smart_mingle/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DJANGO_ENV") == 'dev'
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://smart-mingle-00648c22d190.herokuapp.com/',
@@ -175,10 +175,10 @@ CSRF_TRUSTED_ORIGINS = [
     '127.0.0.1:8000'
 ]
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+if EMAIL_BACKEND == 'django.core.mail.backends.smtp.EmailBackend':
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
